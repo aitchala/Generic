c959eb432b1ba1c5cd3fae1adf93310b41b3e7e8
alexelec <alexelecv5@gmail.com>
2018-01-09 16:27:35 +0200

    oem: modified for the future

--- packages/oem/package.mk
+++ packages/oem/package.mk
@@ -1,31 +1,26 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="oem"
 PKG_VERSION=""
 PKG_ARCH="any"
 PKG_LICENSE="various"
-PKG_SITE="http://www.libreelec.tv"
+PKG_SITE="http://alexelec.in.ua"
 PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain $PROJECT"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="virtual"
 PKG_SHORTDESC="OEM: Metapackage for various OEM packages"
 PKG_LONGDESC="OEM: Metapackage for various OEM packages"
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+# tv services
+#  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET tvheadend oscam"
+
+# services net
+#  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET transmission"
+
+# tools
+#  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET ImageMagick mc scan-m3u serviceref htop"
